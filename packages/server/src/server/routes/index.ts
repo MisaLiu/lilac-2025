9c52193a4a54ecea47c7e414a0311b2755ee1a9c
Misa Liu <misaliu@misaliu.top>
2025-07-25 16:31:31 +0800

    feat(server): Add API error handler

--- packages/server/src/server/routes/index.ts
+++ packages/server/src/server/routes/index.ts
@@ -1,8 +1,22 @@
-import { Router } from 'express';
+import { Router, ErrorRequestHandler } from 'express';
 import Collection from './collection.js';
 
 const router = Router();
 
 router.use('/collection', Collection);
+
+// API error handler
+
+router.use(((err, req, res, next) => {
+  let msg: string = 'Internal server error';
+
+  if (typeof err === 'string') msg = err;
+
+  console.log(typeof err);
+
+  res
+    .status(500)
+    .json({ msg });
+}) as ErrorRequestHandler);
 
 export default router;
