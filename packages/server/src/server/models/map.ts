2886b72807501a9cb7fd1ab9e908e575dfe960c8
Misa Liu <misaliu@misaliu.top>
2025-07-28 19:38:18 +0800

    feat(server): Add map model

---
+++ packages/server/src/server/models/map.ts
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '../database.js';
+
+class Map extends Model {
+  declare id: number;
+  declare name: string;
+  declare parent?: number;
+  declare childrens?: number[];
+};
+
+Map.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    parent: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+    },
+    childrens: {
+      type: DataTypes.JSON,
+      defaultValue: [],
+      allowNull: false,
+    }
+  },
+  {
+    sequelize: db,
+    modelName: 'Map',
+  }
+);
+
+export default Map;
